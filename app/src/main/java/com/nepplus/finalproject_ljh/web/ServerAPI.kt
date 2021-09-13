74811a19be4c75482f69e0133824bbaf3c9b0393
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-13 13:06:03 +0900

    모든 API 요청이 가기전에 토큰을 달기

--- app/src/main/java/com/nepplus/finalproject_ljh/web/ServerAPI.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/web/ServerAPI.kt
@@ -1,5 +1,9 @@
 package com.nepplus.finalproject_ljh.web
 
+import android.content.Context
+import com.nepplus.finalproject_ljh.utils.ContextUtil
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,10 +14,26 @@ class ServerAPI {
 
         private var retrofit: Retrofit? = null
 
-        fun getRetrofit(): Retrofit {
+        fun getRetrofit(context: Context): Retrofit {
             if (retrofit == null) {
+
+//                API 요청 발생 -> 인터셉트 -> 헤더 추가 -> API요청 이어감
+                val interceptor = Interceptor {
+                    with(it) {
+
+                        val newRequest = request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getToken(context))
+                            .build()
+
+                        proceed(newRequest)
+                    }
+                }
+
+                val myClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(hostURL)
+                    .client(myClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
