8470fcaddfc37ff9947fe19bfa8f7797cff43372
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-14 18:16:11 +0900

    네이버 지도 추가

--- app/src/main/java/com/nepplus/finalproject_ljh/EditMyPlaceActivity.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/EditMyPlaceActivity.kt
@@ -3,11 +3,17 @@ package com.nepplus.finalproject_ljh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 import com.nepplus.finalproject_ljh.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditMyPlaceBinding
+
+    var mSelectedLat = 0.0
+    var mSelectedLng = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +25,42 @@ class EditMyPlaceActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.saveBtn.setOnClickListener {
+
+            val inputName = binding.nameEdt.text.toString()
+
+
+
+        }
+
     }
 
     override fun setValues() {
 
         titleTxt.text = "내 출발장소 추가"
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.mapFragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.mapFragment, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+
+            val marker = Marker()
+
+            it.setOnMapClickListener { pointF, latLng ->
+
+                mSelectedLat = latLng.latitude
+                mSelectedLng = latLng.longitude
+
+                marker.position = LatLng(mSelectedLat, mSelectedLng)
+
+                marker.map = it
+
+            }
+
+        }
 
     }
 }
