e10e82ffc2ebb134a843b78dff0a327ec01c077a
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-15 10:41:46 +0900

    리사이클러뷰 LayoutManager 설정

--- app/src/main/java/com/nepplus/finalproject_ljh/ViewMyPlaceListActivity.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/ViewMyPlaceListActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nepplus.finalproject_ljh.adapters.MyPlaceRecyclerAdapter
 import com.nepplus.finalproject_ljh.databinding.ActivityViewMyPlaceListBinding
 import com.nepplus.finalproject_ljh.datas.BasicResponse
 import com.nepplus.finalproject_ljh.datas.PlaceData
@@ -17,6 +21,8 @@ class ViewMyPlaceListActivity : BaseActivity() {
     lateinit var binding: ActivityViewMyPlaceListBinding
 
     val mMyPlaceList = ArrayList<PlaceData>()
+
+    lateinit var mPlaceAdapter: MyPlaceRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +53,10 @@ class ViewMyPlaceListActivity : BaseActivity() {
         titleTxt.text = "내가 자주 쓰는 출발장소들"
         addBtn.visibility = View.VISIBLE
 
+        mPlaceAdapter = MyPlaceRecyclerAdapter(mContext, mMyPlaceList)
+        binding.myPlaceRecyclerView.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false)
+        binding.myPlaceRecyclerView.adapter = mPlaceAdapter
+
     }
 
     fun getMyPlaceListFromServer() {
@@ -59,6 +69,7 @@ class ViewMyPlaceListActivity : BaseActivity() {
                     val basicResponse = response.body()!!
                     mMyPlaceList.clear()
                     mMyPlaceList.addAll(basicResponse.data.places)
+                    mPlaceAdapter.notifyDataSetChanged()
 
                 }
 
