ac131cad3a309d06d0ea69628f5221ce401dc544
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-15 10:53:31 +0900

    리사이클러뷰의 이벤트 처리

--- app/src/main/java/com/nepplus/finalproject_ljh/adapters/MyPlaceRecyclerAdapter.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/adapters/MyPlaceRecyclerAdapter.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.nepplus.finalproject_ljh.R
 import com.nepplus.finalproject_ljh.databinding.ActivityViewMyPlaceListBinding
@@ -25,11 +23,17 @@ class MyPlaceRecyclerAdapter(val mContext: Context, val mList: List<PlaceData>) 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-        val data = mList[position]
+        //val data = mList[position]
 
-        holder.placeNameTxt.text = data.name
-        holder.isPrimaryTxt.text = data.isPrimary.toString()
-        //holder.viewPlaceMapBtn.setImageResource()
+        holder.setRealData(mList[position])
+        
+        holder.viewPlaceMapBtn.setOnClickListener {
+            Toast.makeText(mContext, "지도 버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+        
+        holder.backgroundLayout.setOnClickListener {
+            Toast.makeText(mContext, "${mList[position].name} 클릭됨", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
@@ -40,6 +44,18 @@ class MyPlaceRecyclerAdapter(val mContext: Context, val mList: List<PlaceData>) 
         val placeNameTxt = view.findViewById<TextView>(R.id.placeNameTxt)
         val isPrimaryTxt = view.findViewById<TextView>(R.id.isPrimaryTxt)
         val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
+        val backgroundLayout = view.findViewById<LinearLayout>(R.id.backgroundLayout)
+
+        fun setRealData(data: PlaceData) {
+            placeNameTxt.text = data.name
+            if (data.isPrimary)
+                isPrimaryTxt.visibility = View.VISIBLE
+            else
+                isPrimaryTxt.visibility = View.GONE
+
+            //holder.viewPlaceMapBtn.setImageResource()
+            
+        }
 
     }
 
