d807251c3eeff75c6997d51b735dc2f6645fbdec
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-16 12:55:59 +0900

    리사이클러뷰 어댑터 기초

---
+++ app/src/main/java/com/nepplus/finalproject_ljh/adapters/MyFriendsRecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.nepplus.finalproject_ljh.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nepplus.finalproject_ljh.R
+import com.nepplus.finalproject_ljh.datas.UserResponse
+
+class MyFriendsRecyclerAdapter(val mContext: Context, val mList: List<UserResponse>) : RecyclerView.Adapter<MyFriendsRecyclerAdapter.FriendViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.friend_list_item, parent, false)
+        return FriendViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
+
+        holder.bind()
+
+    }
+
+    override fun getItemCount(): Int = mList.size
+
+    class FriendViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val friendProfileImg = view.findViewById<ImageView>(R.id.friendProfileImg)
+        val nicknameTxt = view.findViewById<TextView>(R.id.nicknameTxt)
+        val socialLoginImg = view.findViewById<ImageView>(R.id.socialLoginImg)
+
+        fun bind(context: Context, data: UserResponse) {
+
+        }
+
+    }
+
+}
