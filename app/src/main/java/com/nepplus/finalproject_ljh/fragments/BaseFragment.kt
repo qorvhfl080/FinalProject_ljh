f9b345b3e7ed0ee9fc6384337961518dcd252ba9
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-16 12:43:11 +0900

    BaseFragment에서 레트로핏 변수 추가

--- app/src/main/java/com/nepplus/finalproject_ljh/fragments/BaseFragment.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/fragments/BaseFragment.kt
@@ -3,14 +3,22 @@ package com.nepplus.finalproject_ljh.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.nepplus.finalproject_ljh.web.ServerAPI
+import com.nepplus.finalproject_ljh.web.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
 
+    private lateinit var retrofit: Retrofit
+    lateinit var apiService: ServerAPIService
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext = requireContext()
+        retrofit = ServerAPI.getRetrofit(mContext)
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
