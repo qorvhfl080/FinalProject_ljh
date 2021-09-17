9a0a265150448b912c240931a38096555f713403
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-17 14:44:40 +0900

    약속 상세화면으로 이동하는 코드

--- app/src/main/java/com/nepplus/finalproject_ljh/adapters/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/adapters/AppointmentRecyclerAdapter.kt
@@ -7,9 +7,11 @@ import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nepplus.finalproject_ljh.R
+import com.nepplus.finalproject_ljh.ViewAppointmentDetailActivity
 import com.nepplus.finalproject_ljh.ViewMapActivity
 import com.nepplus.finalproject_ljh.datas.AppointmentData
 import java.text.SimpleDateFormat
@@ -37,6 +39,7 @@ class AppointmentRecyclerAdapter(val mContext: Context, val mList: List<Appointm
         val dateTimeTxt = view.findViewById<TextView>(R.id.dateTimeTxt)
         val placeNameTxt = view.findViewById<TextView>(R.id.placeNameTxt)
         val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
+        val rootLayout = view.findViewById<LinearLayout>(R.id.rootLayout)
 
         val dateTimeSdf = SimpleDateFormat("M/d a h:mm")
 
@@ -52,6 +55,12 @@ class AppointmentRecyclerAdapter(val mContext: Context, val mList: List<Appointm
                 myIntent.putExtra("appointment", data)
                 context.startActivity(myIntent)
 
+            }
+
+            rootLayout.setOnClickListener {
+                val myIntent = Intent(context, ViewAppointmentDetailActivity::class.java)
+                myIntent.putExtra("appointment", data)
+                context.startActivity(myIntent)
             }
 
         }
