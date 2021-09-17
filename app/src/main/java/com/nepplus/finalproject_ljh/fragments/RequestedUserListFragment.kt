7efbd7990c70fa21a2329fd21125ea5fd12ccc5d
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-17 09:51:35 +0900

    서버에서 친구요청 목록 받아오기

--- app/src/main/java/com/nepplus/finalproject_ljh/fragments/RequestedUserListFragment.kt
+++ app/src/main/java/com/nepplus/finalproject_ljh/fragments/RequestedUserListFragment.kt
@@ -6,12 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nepplus.finalproject_ljh.R
+import com.nepplus.finalproject_ljh.adapters.RequestUserRecyclerAdapter
 import com.nepplus.finalproject_ljh.databinding.FragmentRequestedUserListBinding
+import com.nepplus.finalproject_ljh.datas.BasicResponse
+import com.nepplus.finalproject_ljh.datas.UserResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RequestedUserListFragment : BaseFragment() {
 
     lateinit var binding: FragmentRequestedUserListBinding
+
+    val mRequestUserList = ArrayList<UserResponse>()
+
+    lateinit var mRecyclerAdapter: RequestUserRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,11 +41,41 @@ class RequestedUserListFragment : BaseFragment() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        getRequestUserListFromServer()
+    }
+
     override fun setupEvents() {
 
     }
 
     override fun setValues() {
 
+        mRecyclerAdapter = RequestUserRecyclerAdapter(mContext, mRequestUserList)
+        binding.requestUserRecyclerView.layoutManager = LinearLayoutManager(mContext)
+        binding.requestUserRecyclerView.adapter = mRecyclerAdapter
+
     }
+
+    fun getRequestUserListFromServer() {
+        apiService.getRequestFriendList("requested").enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+
+                    val basicResponse = response.body()!!
+
+                    mRequestUserList.clear()
+                    mRequestUserList.addAll(basicResponse.data.friends)
+                    mRecyclerAdapter.notifyDataSetChanged()
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
+    }
+
 }
