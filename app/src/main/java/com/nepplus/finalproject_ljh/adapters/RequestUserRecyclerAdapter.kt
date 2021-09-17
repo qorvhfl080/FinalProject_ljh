85bc4a2b3b5cc7504f07b4eb7d3fab7e66899194
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-17 09:46:06 +0900

    친구요청 사용자 목록 어댑터 생성

---
+++ app/src/main/java/com/nepplus/finalproject_ljh/adapters/RequestUserRecyclerAdapter.kt
@@ -0,0 +1,57 @@
+package com.nepplus.finalproject_ljh.adapters
+
+import android.content.Context
+import android.content.DialogInterface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nepplus.finalproject_ljh.AddFriendActivity
+import com.nepplus.finalproject_ljh.R
+import com.nepplus.finalproject_ljh.datas.BasicResponse
+import com.nepplus.finalproject_ljh.datas.UserResponse
+import com.nepplus.finalproject_ljh.web.ServerAPI
+import com.nepplus.finalproject_ljh.web.ServerAPIService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RequestUserRecyclerAdapter(val mContext: Context, val mList: List<UserResponse>) : RecyclerView.Adapter<RequestUserRecyclerAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.request_user_list_item, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+
+        holder.bind(mContext, mList[position])
+
+
+
+    }
+
+    override fun getItemCount(): Int = mList.size
+
+    class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val profileImg = view.findViewById<ImageView>(R.id.profileImg)
+        val nicknameTxt = view.findViewById<TextView>(R.id.nicknameTxt)
+        val socialLoginImg = view.findViewById<ImageView>(R.id.socialLoginImg)
+        val acceptBtn = view.findViewById<Button>(R.id.acceptBtn)
+        val refuseBtn = view.findViewById<Button>(R.id.refuseBtn)
+
+        fun bind(context: Context, data: UserResponse) {
+
+
+
+        }
+
+    }
+
+}
